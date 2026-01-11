185bf4a484c4a1a96c20254454eb551eedadd678
shuyang.xu <shuyang.xu@beyonca.com>
2026-01-11 13:01:30 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      // history模式需要通配所有路由，详见vue-router文档
+      path: '/app/app-vue3/:pathMatch(.*)*',
+      name: 'app-vue3',
+      component: () => import('../views/SubContainer.vue'),
+    }
+  ],
+})
+
+export default router
